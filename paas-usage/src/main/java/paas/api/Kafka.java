805a6c1ae0c59f40b06c20f741652060756ee7ad
412418908 <412418908@qq.com>
2018-06-30 16:30:33 +0800

    bootstraplearn

---
+++ paas-usage/src/main/java/paas/api/Kafka.java
@@ -0,0 +1,13 @@
+package paas.api;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+import kafka.serializer.StringEncoder;
+
+
+public class Kafka {
+}
